1172b6c31e88c6768011a1dedd44ac801474ba7f
12max345 <brocoolguf@gmail.com>
2023-08-24 15:30:54 -0700

    Added several files

---
+++ VSCode/Homework/HW01/RandomNumbers.cpp
@@ -0,0 +1,19 @@
+/*********************************************************
+ * AUTHOR : Aden Malikyar
+ * Assignment #1: Random Numbers
+ * CLASS : CS1C
+ * SECTION : TTH: 1:30p - 3:50p
+ * Due Date : 8/29/23
+ *********************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "test";
+
+    return 0;
+}
---
+++ VSCode/template.cpp
@@ -0,0 +1,43 @@
+/*********************************************************
+ * AUTHOR : Aden Malikyar
+ * Assignment #1: Template
+ * CLASS : CS1B
+ * SECTION : TTh: 1:30p – 5:50p
+ * Due Date : 1/5/12
+ *********************************************************/
+#include <iostream>
+using namespace std;
+/**********************************************************
+ *
+ * ADD TWO INTS
+ *_________________________________________________________
+ * This program accepts two integers in from a user, sums
+ * them and then outputs the result to the monitor.
+ *_________________________________________________________
+ * INPUT:
+ * inp1: First integer to be summed -> input from user
+ * inp2: Second integer to be summed -> input from user
+ *
+ * OUTPUT:
+ * sum: The sum of the two ages
+ ***********************************************************/
+int main()
+{
+    // constants
+    int inp1; // INPUT - First integer to sum
+    int inp2; // INPUT - Second integer to sum
+    int sum;  // CALC & OUT - contains the result of
+    // the sum of two inputs –
+    // output the class heading to the screen
+    cout << "*****************************************\n";
+    cout << " Programmed by: Michele Rousseau\n";
+    cout << " Student ID : 750125\n";
+    cout << " CS1B : MW - 6p-7:30\n";
+    cout << " Lab #1 : Eclipse Tutorial\n";
+    cout << "*****************************************\n";
+    ;
+    // INPUT: A description of what is being input.
+    // PROCESSING: Detail what is being processed.
+    // OUTPUT: Details of what is being output.
+    return 0;
+}
