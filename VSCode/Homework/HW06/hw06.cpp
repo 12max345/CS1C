9b70868aad4ce4fdfb6e8a45d48d70a7fd94575e
12max345 <brocoolguf@gmail.com>
2023-09-21 15:48:31 -0700

    Setup for HW06

---
+++ VSCode/Homework/HW06/hw06.cpp
@@ -0,0 +1,21 @@
+/*********************************************************
+ * AUTHOR : Aden Malikyar
+ * Assignment #6: Recursion
+ * CLASS : CS1C
+ * SECTION : TTH: 1:30p - 3:50p
+ * Due Date : 9/26/23
+ *********************************************************/
+
+// This program will take a string an perform various operations such as reversing and selecting certain elements from them with an array.
+
+// g++ -g -o hw06 hw06.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+};
