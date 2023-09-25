5c6c5598304731ca8b5620a035e28a851d1b27b3
12max345 <brocoolguf@gmail.com>
2023-09-24 18:30:11 -0700

    Prepped hw06 for makefile

--- VSCode/Homework/HW06/hw06.cpp
+++ VSCode/Homework/HW06/hw06.cpp
@@ -15,24 +15,61 @@
 
 using namespace std;
 
+/* METHOD
+string indexReverse(string newString, int i1, int i2);
+
+This method intends on taking in a user inputted string alongside two indices so that they can can be reversed from those two points.
+
+Pre-Condition
+    A string and two indices must be passed through.
+
+Post-Condition
+    The method will return a string that is reversed at the two points specified.
+
+*/
 string indexReverse(string newString, int i1, int i2);
 
 int main()
 {
+    // declared variables
     string userString;
     int index1, index2;
 
+    // prompts user the first time for a line and 3 different times for indices
     cout << "Please enter a new line: " << endl;
-    //getline(cin, userString);
-    userString = "abcdefghijklmnopqrstuvwxyz";
-    cout << endl;
-    cout << endl << "Enter indices to reverse at" << endl;
+    getline(cin, userString);
+    cout << endl
+         << "Enter indices to reverse at" << endl;
     cout << "Index 1: ";
     cin >> index1;
     cout << "Index 2: ";
     cin >> index2;
     cout << endl;
 
+    // displays the string reversed from indexReverse method
+    cout << "String Reversed" << endl;
+    cout << indexReverse(userString, index1, index2);
+
+    cout << endl
+         << endl
+         << "Enter indices to reverse at" << endl;
+    cout << "Index 1: ";
+    cin >> index1;
+    cout << "Index 2: ";
+    cin >> index2;
+    cout << endl;
+
+    cout << "String Reversed" << endl;
+    cout << indexReverse(userString, index1, index2);
+
+    cout << endl
+         << endl
+         << "Enter indices to reverse at" << endl;
+    cout << "Index 1: ";
+    cin >> index1;
+    cout << "Index 2: ";
+    cin >> index2;
+    cout << endl;
 
     cout << "String Reversed" << endl;
     cout << indexReverse(userString, index1, index2);
@@ -42,11 +79,14 @@ int main()
 
 string indexReverse(string newString, int in1, int in2)
 {
-    if(in2 > in1)
+    // checks if the first index is meeting the second index
+    if (in2 > in1)
     {
-        newString[in1] = newString[in2];
-        return indexReverse(newString, in1, in2);
-    } else {
+        swap(newString[in1], newString[in2]);
+        return indexReverse(newString, in1 + 1, in2 - 1);
+    }
+    else
+    {
         return newString;
     }
 };
